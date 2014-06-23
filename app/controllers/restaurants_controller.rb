a7806ecc29bb1b9fa34eaef0cebd58ded0396740
Ted <tedkwasnik@gmail.com>
2014-06-22 20:47:40 -0400

    added list of all places

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,17 @@
-class HomeController < ActionController::Base
+class RestaurantsController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def index
-    @latest_restaurants = Restaurant.all.order("created_at desc").limit(10)
+    @restaurants = Restaurant.all.order("name")
+  end
+
+  def create
+
+  end
+
+  def new
   end
 
 end
